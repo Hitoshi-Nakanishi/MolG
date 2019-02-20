7476ad85906184ff53632f422d79899d2ca89b53
37 <hitoshi.nakanishi12345@gmail.com>
2019-02-20 07:52:44 -0500

    read code and deleted useless space

--- models/vae.py
+++ models/vae.py
@@ -1,9 +1,7 @@
 import numpy as np
 import tensorflow as tf
-
 from models import postprocess_logits
 from utils.layers import multi_dense_layers
-
 
 class GraphVAEModel:
 
@@ -11,17 +9,14 @@ class GraphVAEModel:
                  encoder, decoder, soft_gumbel_softmax=False, hard_gumbel_softmax=False, with_features=True):
         self.vertexes, self.nodes, self.edges, self.embedding_dim, self.encoder, self.decoder = \
             vertexes, nodes, edges, embedding_dim, encoder, decoder
-
         self.training = tf.placeholder_with_default(False, shape=())
         self.variational = tf.placeholder_with_default(variational, shape=())
         self.soft_gumbel_softmax = tf.placeholder_with_default(soft_gumbel_softmax, shape=())
         self.hard_gumbel_softmax = tf.placeholder_with_default(hard_gumbel_softmax, shape=())
         self.temperature = tf.placeholder_with_default(1., shape=())
-
         self.edges_labels = tf.placeholder(dtype=tf.int64, shape=(None, vertexes, vertexes))
         self.nodes_labels = tf.placeholder(dtype=tf.int64, shape=(None, vertexes))
         self.node_features = tf.placeholder(dtype=tf.float32, shape=(None, vertexes, features))
-
         self.rewardR = tf.placeholder(dtype=tf.float32, shape=(None, 1))
         self.rewardF = tf.placeholder(dtype=tf.float32, shape=(None, 1))
         self.adjacency_tensor = tf.one_hot(self.edges_labels, depth=edges, dtype=tf.float32)
@@ -31,14 +26,11 @@ class GraphVAEModel:
             outputs = self.encoder(
                 (self.adjacency_tensor, self.node_features if with_features else None, self.node_tensor),
                 units=encoder_units[:-1], training=self.training, dropout_rate=0.)
-
             outputs = multi_dense_layers(outputs, units=encoder_units[-1], activation=tf.nn.tanh,
                                          training=self.training, dropout_rate=0.)
-
             self.embeddings_mean = tf.layers.dense(outputs, embedding_dim, activation=None)
             self.embeddings_std = tf.layers.dense(outputs, embedding_dim, activation=tf.nn.softplus)
             self.q_z = tf.distributions.Normal(self.embeddings_mean, self.embeddings_std)
-
             self.embeddings = tf.cond(self.variational,
                                       lambda: self.q_z.sample(),
                                       lambda: self.embeddings_mean)
@@ -46,7 +38,6 @@ class GraphVAEModel:
         with tf.variable_scope('decoder'):
             self.edges_logits, self.nodes_logits = self.decoder(self.embeddings, decoder_units, vertexes, edges, nodes,
                                                                 training=self.training, dropout_rate=0.)
-
         with tf.name_scope('outputs'):
             (self.edges_softmax, self.nodes_softmax), \
             (self.edges_argmax, self.nodes_argmax), \
@@ -54,19 +45,16 @@ class GraphVAEModel:
             (self.edges_gumbel_softmax, self.nodes_gumbel_softmax), \
             (self.edges_gumbel_argmax, self.nodes_gumbel_argmax) = postprocess_logits(
                 (self.edges_logits, self.nodes_logits), temperature=self.temperature)
-
             self.edges_hat = tf.case({self.soft_gumbel_softmax: lambda: self.edges_gumbel_softmax,
                                       self.hard_gumbel_softmax: lambda: tf.stop_gradient(
                                           self.edges_gumbel_argmax - self.edges_gumbel_softmax) + self.edges_gumbel_softmax},
                                      default=lambda: self.edges_softmax,
                                      exclusive=True)
-
             self.nodes_hat = tf.case({self.soft_gumbel_softmax: lambda: self.nodes_gumbel_softmax,
                                       self.hard_gumbel_softmax: lambda: tf.stop_gradient(
                                           self.nodes_gumbel_argmax - self.nodes_gumbel_softmax) + self.nodes_gumbel_softmax},
                                      default=lambda: self.nodes_softmax,
                                      exclusive=True)
-
         with tf.name_scope('V_x_real'):
             self.value_logits_real = self.V_x((self.adjacency_tensor, None, self.node_tensor), units=encoder_units)
         with tf.name_scope('V_x_fake'):
@@ -75,12 +63,9 @@ class GraphVAEModel:
     def V_x(self, inputs, units):
         with tf.variable_scope('value', reuse=tf.AUTO_REUSE):
             outputs = self.encoder(inputs, units=units[:-1], training=self.training, dropout_rate=0.)
-
             outputs = multi_dense_layers(outputs, units=units[-1], activation=tf.nn.tanh, training=self.training,
                                          dropout_rate=0.)
-
             outputs = tf.layers.dense(outputs, units=1, activation=tf.nn.sigmoid)
-
         return outputs
 
     def sample_z(self, batch_dim):
--- utils/layers.py
+++ utils/layers.py
@@ -4,16 +4,16 @@ def graph_convolution_layer(inputs, units, training, activation=None, dropout_ra
     adjacency_tensor, hidden_tensor, node_tensor = inputs
     adj = tf.transpose(adjacency_tensor[:, :, :, 1:], (0, 3, 1, 2))
     annotations = tf.concat((hidden_tensor, node_tensor), -1) if hidden_tensor is not None else node_tensor
-    output = tf.stack([tf.layers.dense(inputs=annotations, units=units) for _ in range(adj.shape[1])], 1)
+    output = tf.stack([tf.layers.dense(annotations, units) for _ in range(adj.shape[1])], 1)
     output = tf.matmul(adj, output)
-    output = tf.reduce_sum(output, 1) + tf.layers.dense(inputs=annotations, units=units)
+    output = tf.reduce_sum(output, 1) + tf.layers.dense(annotations, units)
     output = activation(output) if activation is not None else output
     output = tf.layers.dropout(output, dropout_rate, training=training)
     return output
 
 def graph_aggregation_layer(inputs, units, training, activation=None, dropout_rate=0.):
-    i = tf.layers.dense(inputs, units=units, activation=tf.nn.sigmoid)
-    j = tf.layers.dense(inputs, units=units, activation=activation)
+    i = tf.layers.dense(inputs, units, activation=tf.nn.sigmoid)
+    j = tf.layers.dense(inputs, units, activation=activation)
     output = tf.reduce_sum(i * j, 1)
     output = activation(output) if activation is not None else output
     output = tf.layers.dropout(output, dropout_rate, training=training)
@@ -22,7 +22,7 @@ def graph_aggregation_layer(inputs, units, training, activation=None, dropout_ra
 def multi_dense_layers(inputs, units, training, activation=None, dropout_rate=0.):
     hidden_tensor = inputs
     for u in units:
-        hidden_tensor = tf.layers.dense(hidden_tensor, units=u, activation=activation)
+        hidden_tensor = tf.layers.dense(hidden_tensor, u, activation=activation)
         hidden_tensor = tf.layers.dropout(hidden_tensor, dropout_rate, training=training)
     return hidden_tensor
 
